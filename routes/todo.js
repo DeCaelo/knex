8b26ac830eb43ebfac83385245605e4dbc845887
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-03-15 09:20:29 +0100

    create a record with post /todo

--- routes/todo.js
+++ routes/todo.js
@@ -12,9 +12,47 @@ router.get('/', (req, res) => {
     });
 });
 
-// new todo
+// new todo route
 router.get('/new', (req, res) => {
   res.render('new');
 });
+
+// create new todo
+router.post('/', (req, res) => {
+  validateTodoRenderError(req, res, (todo) => {
+    todo.date = new Date();
+    // insert into the database
+    knex('todo')
+      .insert(todo, 'id')
+      .then(ids => {
+        const id = ids[0];
+        res.redirect(`/todo/${id}`);
+      });
+  });
+});
+
+function validateTodoRenderError(req, res, callback) {
+  if(validTodo(req.body)) {
+    const todo = {
+      title: req.body.title,
+      description: req.body.description,
+      priority: req.body.priority
+    };
+
+    callback(todo);
+  } else {
+    res.status( 500);
+    res.render('error', {
+      message:  'Invalid todo'
+    });
+  }
+}
+
+function validTodo(todo) {
+  return typeof todo.title == 'string' &&
+          todo.title.trim() != '' &&
+          typeof todo.priority != 'undefined' &&
+          !isNaN(todo.priority);
+}
 
 module.exports = router;
