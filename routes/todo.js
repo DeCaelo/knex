be84b3d175d056967be6a2420113509993a6559d
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-03-15 09:00:05 +0100

    show new form with /todo/new

--- routes/todo.js
+++ routes/todo.js
@@ -4,12 +4,17 @@ var router = express.Router();
 const knex = require('../db/knex');
 
 /* This router is mounted at http://localhost:3000/todo */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   knex('todo')
     .select()
     .then(todos => {
-      res.render('all', { todos: todos })
+      res.render('all', { todos: todos });
     });
+});
+
+// new todo
+router.get('/new', (req, res) => {
+  res.render('new');
 });
 
 module.exports = router;
